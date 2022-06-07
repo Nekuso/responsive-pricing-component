5a374a127571ef1ffdc4df558c4e9298b96a3223
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-06-08 00:40:35 +0800

    added files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,10 @@ value.textContent = slider.value;
 slider.oninput = function() {
     value.textContent = this.value;
 }
+
+slider.addEventListener('input', function(){
+    var x = slider.value * 3;
+    var color = 'linear-gradient(90deg, hsl(174, 77%, 80%)' + x +'%, rgb(214,214,214)' + x + '%)';
+    slider.style.background = color;
+
+})
