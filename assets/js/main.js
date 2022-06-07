9790ff1777b87fd07d5b35237e7bb5a7f0bc0fd9
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-06-08 01:40:57 +0800

    added files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@ const value = document.querySelector(".price");
 value.textContent = slider.value;
 
 slider.oninput = function() {
-    value.textContent = this.value;
+    value.textContent = this.value + ".00";
 }
 
 slider.addEventListener('input', function(){
     var x = slider.value * 3;
-    var color = 'linear-gradient(90deg, hsl(174, 77%, 80%)' + x +'%, rgb(214,214,214)' + x + '%)';
+    var color = 'linear-gradient(90deg, hsl(174, 77%, 80%)' + x +'%, hsl(223, 50%, 87%)' + x + '%)';
     slider.style.background = color;
 
 })
